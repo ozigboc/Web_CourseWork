d94b3701484ec8ebac5d4eda787879ef2c92ceb4
ozigboc <ozigboc@gmail.com>
2024-03-03 17:14:05 +0000

    Yaml file updated

--- obj/Debug/net8.0/Web_CourseWork.AssemblyInfo.cs
+++ obj/Debug/net8.0/Web_CourseWork.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_CourseWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f92d3ff0973b17592200162b02379129ebc486d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3b957a85e4a30480c85ad8bb193478f44f81ae2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_CourseWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_CourseWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
